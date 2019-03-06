84b3701333912940ec38ec7615060dfc68f79f70
Yi-Shin lin <yishinlin@outlook.com>
2019-03-06 12:05:23 +0800

    Updates

---
+++ precache-manifest.9d72e43be716227595cd4b551f11a7bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b85ec09627a971406a20",
+    "url": "/react-try/static/js/runtime~main.b85ec096.js"
+  },
+  {
+    "revision": "9535d10557d74af96613",
+    "url": "/react-try/static/js/main.9535d105.chunk.js"
+  },
+  {
+    "revision": "cb016095e848669d37ac",
+    "url": "/react-try/static/js/2.cb016095.chunk.js"
+  },
+  {
+    "revision": "9535d10557d74af96613",
+    "url": "/react-try/static/css/main.5fb987ec.chunk.css"
+  },
+  {
+    "revision": "6b4bef86b1cdc2ae211de43feea50a2a",
+    "url": "/react-try/index.html"
+  }
+];
